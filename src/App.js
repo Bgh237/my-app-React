673512f7dbff5013a6a2d8df48c45a85616a6abe
Bgh237 <58211837+Bgh237@users.noreply.github.com>
2020-03-13 11:36:13 +0200

    material-ui features added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React, {useState} from 'react';
 import './App.css';
 import ReactTable from 'react-table-v6'
 import 'react-table-v6/react-table.css'
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import Tooltip from '@material-ui/core/Tooltip';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
       
-      const [todo, setTodo] = useState({desc: '', date: ''});
+      const [todo, setTodo] = useState({desc: '', date: new Date()});
       const [todos, setTodos] = useState([]);
 
       const addTodo = (event) => {
         event.preventDefault();
-        setTodos([{desc: todo.desc, date: todo.date}, ...todos])
+        setTodos([{desc: todo.desc, date: todo.date.toLocaleDateString()}, ...todos])
       }
 
       const inputChanged = (event) => {
@@ -20,6 +36,9 @@ function App() {
       const deleteTodo = (row) => {
         setTodos(todos.filter((_todo, index) => row !== index))
       }
+      const handleDateChange = date => {
+        setTodo({...todo, date: date});
+      };
 
       const columns = [
         {
@@ -32,7 +51,7 @@ function App() {
         },
         {
           Cell: row => (
-              <button onClick={() => deleteTodo(row.index)}>Delete</button>
+              <Button color="secondary" size="small" onClick={() => deleteTodo(row.index)}>Delete</Button>
           )
         }
       ]
@@ -40,13 +59,26 @@ function App() {
       return (
         
         <div className="App">
-          <header className="App-header">
-          Simple Todolist
-          </header>
+          <AppBar position="static">
+        <Toolbar>
+          <Typography style={{flex: 10, alignItems: 'center'}} variant="h6">
+            My To Do List
+          </Typography>
+        </Toolbar>
+      </AppBar>
           <form onSubmit={addTodo}>
-            Description: <input type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
-            Date: <input type="date" name="date" value={todo.date} onChange={inputChanged}/>
-            <input type="submit" value="Add" />
+            <TextField style={{marginRight: 10}} label="Descpription" name="desc" value={todo.desc} onChange={inputChanged}/>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                format="dd/MM/yyyy"
+                label="Date"
+                value={todo.date}
+                onChange={date => handleDateChange(date)}
+              />            
+            </MuiPickersUtilsProvider>
+            <Tooltip title="Add">
+              <IconButton color="primary" onClick={addTodo}><AddCircleOutlineIcon fontSize="large" /></IconButton>
+            </Tooltip>
           </form>
           <ReactTable data={todos} columns={columns} defaultPageSize={10} filterable={true}/>
           
